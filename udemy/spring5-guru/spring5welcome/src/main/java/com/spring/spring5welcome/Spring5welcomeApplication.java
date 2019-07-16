6c83068b38c44e7e607ba38eab02ae2ec4864c14
bone1289 <bogdan.rata1289@gmail.com>
2019-07-16 14:49:50 +0300

    Intial Commit for Spring 5 Guru

---
+++ udemy/spring5-guru/spring5welcome/src/main/java/com/spring/spring5welcome/Spring5welcomeApplication.java
@@ -0,0 +1,13 @@
+package com.spring.spring5welcome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5welcomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5welcomeApplication.class, args);
+	}
+
+}
