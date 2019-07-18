c62598724c847580820d139180d3dfe5525ab9e0
bone1289 <bogdan.rata1289@gmail.com>
2019-07-18 20:06:43 +0300

    Adding Abstract Map Service and implment for Owner, Pet, Vet.

---
+++ udemy/spring5-guru/sfg-pet-clinic/pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/service/map/VetServiceMap.java
@@ -0,0 +1,34 @@
+package com.springframework.sfgpetclinic.service.map;
+
+import com.springframework.sfgpetclinic.model.Owner;
+import com.springframework.sfgpetclinic.model.Vet;
+import com.springframework.sfgpetclinic.service.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+    @Override
+    public Vet save(Vet vet) {
+        return super.save(vet.getId(), vet);
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet vet) {
+        super.delete(vet);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+}
