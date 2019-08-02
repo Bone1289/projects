d9269c46cb16994e0e597a56a2289c8c7f8bcf58
Rata Bogdan <bogdan.rata1289@gmail.com>
2019-08-02 22:31:01 +0300

    Fix the issue were NoArgsConstructor was req.

--- udemy/spring5-guru/spring-recipe/src/main/java/com/springboot/recipe/domain/Recipe.java
+++ udemy/spring5-guru/spring-recipe/src/main/java/com/springboot/recipe/domain/Recipe.java
@@ -1,6 +1,7 @@
 package com.springboot.recipe.domain;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.util.HashSet;
@@ -8,6 +9,7 @@ import java.util.Set;
 
 @Data
 @Entity
+@NoArgsConstructor
 public class Recipe {
 
     @Id
