f458f7b2593b41e1473dc8e22635940a85781720
bone1289 <bogdan.rata1289@gmail.com>
2019-08-10 09:58:01 +0300

    Adding Command objets.
    Fixed the IngredientCommand

---
+++ udemy/spring5-guru/spring-recipe/src/main/java/com/springboot/recipe/commands/CategoryCommand.java
@@ -0,0 +1,13 @@
+package com.springboot.recipe.commands;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+public class CategoryCommand {
+    private Long id;
+    private String descriptsion;
+}
