a5a82bd59ba25f002c5c4b113c4a505bed0dba30
bone1289 <bogdan.rata1289@gmail.com>
2019-08-10 10:35:42 +0300

    Adding Category <-> Command.
    Add Category <-> Command Converter test.

---
+++ udemy/spring5-guru/spring-recipe/src/test/java/com/springboot/recipe/converers/CategoryToCategoryCommandTest.java
@@ -0,0 +1,45 @@
+package com.springboot.recipe.converers;
+
+import com.springboot.recipe.commands.CategoryCommand;
+import com.springboot.recipe.domain.Category;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CategoryToCategoryCommandTest {
+    private static final Long ID_VALUE = 1L;
+    private static final String DESCRIPTION = "description";
+    CategoryToCategoryCommand converter;
+
+    @Before
+    public void setUp() {
+        converter = new CategoryToCategoryCommand();
+    }
+
+
+    @Test
+    public void testNullObject() {
+        assertNull(converter.convert(null));
+    }
+
+    @Test
+    public void testEmptyObject() {
+        assertNotNull(converter.convert(new Category()));
+    }
+
+    @Test
+    public void convert() {
+        //given
+        Category category = new Category();
+        category.setId(ID_VALUE);
+        category.setDescription(DESCRIPTION);
+
+        //when
+        CategoryCommand categoryCommand = converter.convert(category);
+
+        //then
+        assertEquals(ID_VALUE, categoryCommand.getId());
+        assertEquals(DESCRIPTION, categoryCommand.getDescription());
+    }
+}
