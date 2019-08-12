54494e352d0a432d01036b663875b75aca965f91
bone1289 <bogdan.rata1289@gmail.com>
2019-08-12 21:55:01 +0300

    [Recipe][Request delete] Adding delete funct + test.

--- udemy/spring5-guru/spring-recipe/src/main/java/com/springboot/recipe/controllers/RecipeController.java
+++ udemy/spring5-guru/spring-recipe/src/main/java/com/springboot/recipe/controllers/RecipeController.java
@@ -2,13 +2,12 @@ package com.springboot.recipe.controllers;
 
 import com.springboot.recipe.commands.RecipeCommand;
 import com.springboot.recipe.service.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @Controller
 public class RecipeController {
 
@@ -19,12 +18,14 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
+    @GetMapping
     @RequestMapping("/recipe/{id}/show")
     public String showById(@PathVariable String id, Model model) {
         model.addAttribute("recipe", recipeService.findById(new Long(id)));
         return "recipe/show";
     }
 
+    @GetMapping
     @RequestMapping("recipe/new")
     public String newRecipe(Model model) {
         model.addAttribute("recipe", new RecipeCommand());
@@ -38,9 +39,21 @@ public class RecipeController {
         return "redirect:/recipe/" + recipeCommand.getId() + "/show";
     }
 
+    @GetMapping
     @RequestMapping("recipe/{id}/update")
     public String updateRecipe(@PathVariable String id, Model model) {
         model.addAttribute("recipe", recipeService.findCommandById(new Long(id)));
         return "recipe/recipeform";
     }
+
+    @GetMapping
+    @RequestMapping("recipe/{id}/delete")
+    public String deleteById(@PathVariable String id) {
+
+        log.debug("Deleting id:" + id);
+
+        recipeService.deleteById(new Long(id));
+        return "redirect:/";
+    }
+
 }
--- udemy/spring5-guru/spring-recipe/src/main/java/com/springboot/recipe/service/RecipeService.java
+++ udemy/spring5-guru/spring-recipe/src/main/java/com/springboot/recipe/service/RecipeService.java
@@ -2,13 +2,17 @@ package com.springboot.recipe.service;
 
 import com.springboot.recipe.commands.RecipeCommand;
 import com.springboot.recipe.domain.Recipe;
-import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 public interface RecipeService {
     Set<Recipe> getRecipes();
+
     Recipe findById(Long id);
+
     RecipeCommand saveRecipeCommand(RecipeCommand command);
+
     RecipeCommand findCommandById(Long id);
+
+    void deleteById(Long recipeId);
 }
