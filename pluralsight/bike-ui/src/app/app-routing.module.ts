9c4265548046e0cde406241dbe1382c1b7098753
bogdrata <bogdan.rata1289@gmail.com>
2019-10-25 12:05:07 +0300

    Add proxy and setup.

--- pluralsight/bike-ui/src/app/app-routing.module.ts
+++ pluralsight/bike-ui/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
+import { AdminComponent } from './components/admin/admin.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'admin',
+  component: AdminComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
