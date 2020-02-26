587077910a4008e906bef1bd07214e63063b5296
bone1289 <bogdan.rata1289@gmail.com>
2020-02-26 21:56:30 +0200

    Netify - Init Project

---
+++ udemy/spring5-reactive/Netify/src/main/java/com/reactive/netify/NetifyApplication.java
@@ -0,0 +1,13 @@
+package com.reactive.netify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetifyApplication.class, args);
+	}
+
+}
---
+++ udemy/spring5-reactive/Netify/src/test/java/com/reactive/netify/NetifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactive.netify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
