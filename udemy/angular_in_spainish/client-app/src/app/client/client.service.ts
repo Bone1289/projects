2f24987245a9fe528cfdd5c53cf3809de7e92c9f
Rata Bogdan <bogdan.rata1289@gmail.com>
2020-04-22 17:12:45 +0300

    [WebGui] Modify client service to use backend.

--- udemy/angular_in_spainish/client-app/src/app/client/client.service.ts
+++ udemy/angular_in_spainish/client-app/src/app/client/client.service.ts
@@ -2,16 +2,20 @@ import {Injectable} from '@angular/core';
 import {CLIENTES} from "./clients.json";
 import {Client} from "./client";
 import {Observable, of} from "rxjs";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientService {
 
-  constructor() {
+  private urlEndpoint:string = "http://localhost:8080/api";
+  private urlClientEndpoint:string= this.urlEndpoint+"/clients";
+
+  constructor(private http:HttpClient) {
   }
 
   getClients(): Observable<Client[]> {
-    return of(CLIENTES);
+    return this.http.get<Client[]>(this.urlClientEndpoint)
   }
 }
