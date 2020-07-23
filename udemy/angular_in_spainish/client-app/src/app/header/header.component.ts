750c41de39743bd6f33c071f42307ce096f6a2bb
Rata Bogdan <bogdan.rata1289@gmail.com>
2020-07-23 17:06:58 +0300

    [FrontEnd] Add logout method.

--- udemy/angular_in_spainish/client-app/src/app/header/header.component.ts
+++ udemy/angular_in_spainish/client-app/src/app/header/header.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from "../users/auth.service";
+import {Router} from "@angular/router";
+import Swal from "sweetalert2";
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  templateUrl: './header.component.html'
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(public authService: AuthService, public route: Router) {
+  }
 
   ngOnInit(): void {
   }
 
+  logout(): void {
+    let username = `${this.authService.user.username}`;
+    this.authService.logout();
+    Swal.fire('Login', `Logout ${username} successfully!`, 'info');
+    this.route.navigate(['/login']);
+  }
 }
