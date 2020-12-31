64e95fdd7039aca690baa19699ff2495c015967b
Rata Bogdan <bogdan.rata1289@gmail.com>
2020-12-31 15:43:52 +0200

    [Udemy][MicroserviceGuru] Beer project init.

---
+++ udemy/microservice-guru/microservice-beer/src/main/java/com/udemy/guru/beer/microservicebeer/MicroserviceBeerApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.guru.beer.microservicebeer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceBeerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceBeerApplication.class, args);
+    }
+
+}
---
+++ udemy/microservice-guru/microservice-beer/src/test/java/com/udemy/guru/beer/microservicebeer/MicroserviceBeerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.guru.beer.microservicebeer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceBeerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
