99ebb8134e11971c22902062703e39cd6fd4f061
Bogdan Rata <bogdan.rata1289@gmail.com>
2021-04-11 11:39:25 +0300

    Project Setup

---
+++ youtube/spring-security/spring-security-1/src/test/java/com/bone/SpringSecurityEx1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityEx1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
