44c317d181f4be9ba1e767fd75fb405b22c35c05
Bogdan Rata <bogdan.rata1289@gmail.com>
2022-01-03 12:52:54 +0200

    Add Stub File

---
+++ data-structure/src/main/java/array/FirstDuplicateValue.java
@@ -0,0 +1,27 @@
+package array;
+
+/**
+ * Given an array of integers between 1 and n, inclusive, where n is the length of the array, write a function that
+ * returns the first integer that appears more than once (when the array is read from left to right).
+ * <p>
+ * In other words, out of all the integers that might occur more than once in the input array, your function should
+ * return the one whose first duplicate value has the minimum index.
+ * <p>
+ * If no integer appears more than once, your function should return -1.
+ *
+ * <p>Note that you're allowed to mutate the input array.</p>
+ * <h3>Sample Input #1</h3>
+ * array = [2, 1, 5, 2, 3, 3, 4]
+ *
+ * <h3>Sample Output #1</h3>
+ * 2 // 2 is the first integer that appears more than once. // 3 also appears more than once, but the second 3 appears
+ * after the second 2.
+ *
+ * <h3>Sample Input #2</h3>
+ * array = [2, 1, 5, 3, 3, 2, 4]
+ * <h3>Sample Output #2</h3>
+ * 3 // 3 is the first integer that appears more than once. // 2 also appears more than once, but the second 2 appears
+ * after the second 3.
+ */
+public class FirstDuplicateValue {
+}
